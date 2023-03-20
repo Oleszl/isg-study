307e70c67cf9f951c152171d3fb8f03ddf659c89
Oles Sukmanovskyi <68856819+Oleszl@users.noreply.github.com>
2023-03-20 12:07:02 +0200

    Update MyInput.jsx
    add required fields

--- my-app/src/components/UI/input/MyInput.jsx
+++ my-app/src/components/UI/input/MyInput.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./MyInput.scss";
 
 const MyInput = (props) => {
-  return <input className="myInput" {...props} />;
+  return <input className="myInput" {...props} required="true"/>;
 };
 
 export default MyInput;
